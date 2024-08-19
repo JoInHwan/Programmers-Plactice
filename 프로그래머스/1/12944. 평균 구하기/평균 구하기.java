62b95376ad852f728a55859c3193d0349a99ec4e
Enan <106017253+JoInHwan@users.noreply.github.com>
2024-08-19 15:23:39 +0900

    [level 1] Title: 평균 구하기, Time: 0.06 ms, Memory: 93.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        int hap = 0;
+        
+        for(int i=0; i<arr.length; i++){
+            hap = hap+arr[i];
+        }
+        answer = (double)hap / arr.length;
+        
+        return answer;
+    }
+}
